ae7b0bc7e3c35255cf18c2abf5f6bcefedcb9adb
yangxueyong <yangxueyong@nantian.com.cn>
2023-05-30 11:13:39 +0800

    新增判断字符串是否为空的方法

--- version-demo/src/main/java/com/example/yxy/utils/StringUtil.java
+++ version-demo/src/main/java/com/example/yxy/utils/StringUtil.java
@@ -1,4 +1,10 @@
 package com.example.yxy.utils;
 
 public class StringUtil {
+    public static boolean isNull(String s){
+        if(s == null){
+            return true;
+        }
+        return false;
+    }
 }
