39494602c97a5db3114cda97400d67478c3fc762
yangxueyong <yangxueyong@nantian.com.cn>
2023-05-30 11:11:09 +0800

    新增版本控制demo

---
+++ version-demo/src/main/java/com/example/yxy/VersionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.yxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersionDemoApplication.class, args);
+    }
+
+}
---
+++ version-demo/src/main/java/com/example/yxy/utils/StringUtil.java
@@ -0,0 +1,4 @@
+package com.example.yxy.utils;
+
+public class StringUtil {
+}
