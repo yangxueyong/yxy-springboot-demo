416c7a126bfd0580dc41eaa5de961e9c30b7f0e2
yangxueyong <yangxueyong@nantian.com.cn>
2023-06-05 17:16:32 +0800

    格式化sql语句

--- flinksql-demo/src/main/resources/batch_flink_init.sql
+++ flinksql-demo/src/main/resources/batch_flink_init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE t_p_cust_deposit_prod (
-                                       cust_no String,
-                                       main_prod_no String,
-                                       sub_prod_no String,
-                                       amount decimal(30,2) ,
-                                       begin_time TIMESTAMP ,
-                                       end_time TIMESTAMP
+   cust_no String,
+   main_prod_no String,
+   sub_prod_no String,
+   amount decimal(30,2) ,
+   begin_time TIMESTAMP ,
+   end_time TIMESTAMP
 ) with(
    'connector' = 'jdbc',
    'url' = 'jdbc:mysql://127.0.0.1:3316/test_flink?useSSL=false&serverTimezone=Asia/Shanghai&allowMultiQueries=true&allowPublicKeyRetrieval=true',
@@ -13,12 +13,12 @@ CREATE TABLE t_p_cust_deposit_prod (
    'password'='123456789'
 );
 CREATE TABLE t_p_cust_loan_prod (
-                                    cust_no String,
-                                    main_prod_no String,
-                                    sub_prod_no String,
-                                    amount decimal(30,2) ,
-                                    begin_time TIMESTAMP ,
-                                    end_time TIMESTAMP
+    cust_no String,
+    main_prod_no String,
+    sub_prod_no String,
+    amount decimal(30,2) ,
+    begin_time TIMESTAMP ,
+    end_time TIMESTAMP
 ) with(
    'connector' = 'jdbc',
    'url' = 'jdbc:mysql://127.0.0.1:3316/test_flink?useSSL=false&serverTimezone=Asia/Shanghai&allowMultiQueries=true&allowPublicKeyRetrieval=true',
@@ -27,10 +27,10 @@ CREATE TABLE t_p_cust_loan_prod (
    'password'='123456789'
 );
 CREATE TABLE t_p_cust_prod (
-                               cust_no String,
-                               main_prod_no String,
-                               num decimal(30,2),
-                               amount decimal(30,2) ,primary key(cust_no,main_prod_no) not enforced
+   cust_no String,
+   main_prod_no String,
+   num decimal(30,2),
+   amount decimal(30,2) ,primary key(cust_no,main_prod_no) not enforced
 ) with(
    'connector' = 'jdbc',
    'url' = 'jdbc:mysql://127.0.0.1:3316/test_flink?useSSL=false&serverTimezone=Asia/Shanghai&allowMultiQueries=true&allowPublicKeyRetrieval=true',
