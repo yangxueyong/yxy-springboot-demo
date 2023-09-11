cd4fe45da2fdc83c6ed80c6b13bd7a805d5eb575
yangxueyong <yangxueyong@nantian.com.cn>
2023-09-11 10:38:46 +0800

    commit2

---
+++ version-demo/src/main/java/com/example/yxy/entity/Test4.java
@@ -0,0 +1,28 @@
+package com.example.yxy.entity;
+
+public class Test4 {
+
+    /**
+     * id : xx
+     * name : 张三
+     */
+
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
