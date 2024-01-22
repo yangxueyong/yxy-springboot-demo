f565a65aad1a8cd385e547c0986ca50f992ba708
yangxueyong <yangxueyong@nantian.com.cn>
2024-01-22 19:08:43 +0800

    t1 第2次提交

--- simple-springboot/src/main/java/com/example/version1/Test1.java
+++ simple-springboot/src/main/java/com/example/version1/Test1.java
@@ -3,5 +3,7 @@ package com.example.version1;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("test1");
+
+        System.out.println("test2");
     }
 }
