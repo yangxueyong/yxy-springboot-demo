3a64d1042e9ec911b85fd567fa2e6547db701b30
yangxueyong <yangxueyong@nantian.com.cn>
2024-01-22 17:36:22 +0800

    test2提交2

--- simple-springboot/src/main/java/com/example/yxy/service/testversion2/Test2.java
+++ simple-springboot/src/main/java/com/example/yxy/service/testversion2/Test2.java
@@ -3,5 +3,7 @@ package com.example.yxy.service.testversion2;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2");
+
+        System.out.println("test2 2");
     }
 }
